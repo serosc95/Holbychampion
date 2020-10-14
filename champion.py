2d2f871f8e5c264fba7a4f3ee6045ef5302063fe
Andres802 <reachandres@gmail.com>
2020-10-14 17:03:18 +0000

    private def

--- champion.py
+++ champion.py
@@ -4,15 +4,25 @@ import json
 
 class Champion:
     def __init__(self, name, race, gender, level=0, exp_next_level=0, currente_exp=0, total_exp=0, stats=0, stat_points=0):
-        self.name = name
-        self.race = race
-        self.gender = gender
-        self.level = level
+        self.__name = name
+        self.__race = race
+        self.__gender = gender
+        self.__level = level
         self.exp_next_level = exp_next_level
         self.currente_exp = currente_exp
         self.total_exp = total_exp
         self.stats = stats
         self.stat_points = stat_points
+    
+    def name(self):
+        return self.__name
+
+    def race(self):
+        return self.__race
+    
+    def gender(self):
+        return self.__gender
+
 
     def level_up(self):
 
